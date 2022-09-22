1530175dcb9bb239e8bccb54278c0e92af7e1f3b
KA�RA DA SILVA CARDOSO <kayra.8940@aluno.pr.senac.br>
2022-09-22 19:36:32 -0300

    Válido e inválido

---
+++ Exerciciodecondicional.java
@@ -0,0 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exerciciodecondicional {
+    public static void main(String[] args) {
+        String masc= "M";
+        String fem= "F";
+        Scanner genero = new Scanner(System.in);
+        System.out.println("Digite a letra M ou F:");
+        String qualquer= genero.next();
+        if (masc.equals(qualquer)) {
+            String generodigitado= qualquer.toUpperCase(Locale.ROOT);
+            // ou if (qualquer.equals(masc)||qualquer.equals(fem))
+
+            System.out.println("O genero e valido!");
+        }
+        else if(fem.equals(qualquer)){
+            System.out.println("O genero e valido!");
+        } else
+        {System.out.println("Genero invalido!");
+        }
+    }}
