556ad8741fbac5191622fcfd4c7e1737c9099e9d
KA�RA DA SILVA CARDOSO <kayra.8940@aluno.pr.senac.br>
2022-09-29 20:46:50 -0300

    ComandosAOcarro

---
+++ ExercicioVetor.java
@@ -0,0 +1,28 @@
+public class ExercicioVetor {
+    public static void main(String[] args) {
+        String[] InformacoesPaises=new String[20];
+        InformacoesPaises[0]="Brasil";
+        InformacoesPaises[1]="Chile";
+        InformacoesPaises[2]="Equador";
+        InformacoesPaises[3]="Bolivia";
+        InformacoesPaises[4]="Estados Unidos";
+        InformacoesPaises[5]="Canada";
+        InformacoesPaises[6]="Groelandia";
+        InformacoesPaises[7]="Siria";
+        InformacoesPaises[8]="Peru";
+        InformacoesPaises[9]="Italia";
+        InformacoesPaises[10]="Alemanha";
+        InformacoesPaises[11]="Russia";
+        InformacoesPaises[12]="Inglaterra";
+        InformacoesPaises[13]="Filipinas";
+        InformacoesPaises[14]="China";
+        InformacoesPaises[15]="Japao";
+        InformacoesPaises[16]="Ucrania";
+        InformacoesPaises[17]="Franca";
+        InformacoesPaises[18]="Egito";
+        InformacoesPaises[19]="Cuba";
+
+
+
+    }
+}
